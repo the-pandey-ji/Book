21b4c152591c722f0f202104b4d91badf8a614d1
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-09-18 21:18:21 +0530

    create parameterised constructor of class : Book

--- src/main/Book.java
+++ src/main/Book.java
@@ -10,4 +10,10 @@ public class Book {
         return "-----------------------------\n" + "Book Name:\t" + bookName + "\nAuthor Name:\t" + authorName + "/nISBN:\t" + isbn +
                 "\n-----------------------------\n";
     }
+
+    Book(String bookName, String authorName, String isbn) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.isbn = isbn;
+    }
 }
