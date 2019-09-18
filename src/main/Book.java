e20404fd399880dfa9395fbd26cc25b85d44f552
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-09-18 21:15:23 +0530

    override function : toString

--- src/main/Book.java
+++ src/main/Book.java
@@ -4,4 +4,10 @@ public class Book {
     private String bookName;
     private String authorName;
     private String isbn;
+
+    @Override
+    public String toString() {
+        return "-----------------------------\n" + "Book Name:\t" + bookName + "\nAuthor Name:\t" + authorName + "/nISBN:\t" + isbn +
+                "\n-----------------------------\n";
+    }
 }
