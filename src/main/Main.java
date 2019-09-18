73723fa695acc4b726bb973eef9ca6ece72dc732
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-09-18 21:31:27 +0530

    create array:book of objects of class : Book

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Book[] book = new Book[n];
+
     }
 }
