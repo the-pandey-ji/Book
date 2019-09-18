8f2ebd5a8eaa7b5c518b68bbf3dd0e3ea3b8c0af
Warrior <sonu.pandey_cs18@gla.ac.in>
2019-09-18 21:39:32 +0530

    apply logic

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String bookName;
+        String authorName;
+        String isbn;
         int n = scan.nextInt();
         Book[] book = new Book[n];
+        if (n == 0) {
+            System.out.println("N/A");
+        } else {
+            for (int i = 0; i < n; i++) {
+                bookName = scan.nextLine();
+                authorName = scan.nextLine();
+                isbn = scan.nextLine();
+                book[i] = new Book(bookName, authorName, isbn);
+
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.println(book[i]);
+            }
+        }
+        scan.close();
 
     }
 }
